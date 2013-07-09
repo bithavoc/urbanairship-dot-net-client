e0a134e6e2ee9525a5527a9b0ec590e580d28ec8
Justin Hyland <Hylander0@hotmail.com>
2013-07-09 10:47:41 -0400

    -Added support for sending push notification to Google Cloud Messaging devices -Added support for registering devices against an alias -Added jsonDictionary Serialization Helper -Added PlatformBase implementations for GCM -updated README.md file with examples

--- UrbanAirship/PlatfromBase.cs
+++ UrbanAirship/PlatfromBase.cs
@@ -30,6 +30,25 @@ namespace UrbanAirship
             }
             this.OnRegisterDevice(token);
         }
+
+
+        /// <summary>
+        /// Register a device token.
+        /// </summary>
+        /// <param name="token">Device token(iOS Device Token, Android APID, etc)</param>
+        public void RegisterDevice(string token, string alias)
+        {
+            if (token == null)
+            {
+                throw new ArgumentNullException("token");
+            }
+            if (String.IsNullOrEmpty(alias))
+            {
+                throw new ArgumentNullException("Alias");
+            }
+            this.OnRegisterDeviceWithAlias(token, alias);
+        }
         protected abstract void OnRegisterDevice(string token);
+        protected abstract void OnRegisterDeviceWithAlias(string token, string alias);
     }
 }
